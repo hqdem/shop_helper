2d658b2d6575808c654ef5c562c18e1e36477f6b
hqdem <ilyadem4@gmail.com>
2022-12-11 13:22:47 +0300

    add endpoints to add and remove products from recipe

--- shop_helper/urls.py
+++ shop_helper/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from .views import *
 from . import routers
 
 urlpatterns = [
+    path('add_product/', AddProductToRecipe.as_view(), name='add_product'),
+    path('remove_product/', RemoveProductToRecipe.as_view(), name='remove_product'),
 ] + routers.urlpatterns
---
+++ shop_helper/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+from .models import Product, Recipe, RecipesProducts
+from .serializers import RecipeSerializer
+
+
+class AddProductToRecipe(APIView):
+    def post(self, request):
+        data = request.data
+        try:
+            recipe_id = data['recipe_id']
+            product_id = data['product_id']
+            count = data['count']
+
+            instance, created = RecipesProducts.objects.get_or_create(recipe_id=recipe_id, product_id=product_id, defaults={'product_count': count})
+            if created:
+                return Response(status=status.HTTP_201_CREATED)
+            instance.product_count = count
+            instance.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        except KeyError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class RemoveProductToRecipe(APIView):
+    def post(self, request):
+        data = request.data
+        try:
+            recipe_id = data['recipe_id']
+            product_id = data['product_id']
+            instance = get_object_or_404(RecipesProducts, recipe_id=recipe_id, product_id=product_id)
+            instance.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except KeyError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
