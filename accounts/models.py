00adf127630405e1c8ce41f309c85298a7563358
hqdem <ilyadem4@gmail.com>
2022-12-15 14:33:21 +0300

    add subscribers field to user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class MyUser(AbstractUser):
-    pass
+    subscribers = models.ManyToManyField('self', symmetrical=False, verbose_name='Подписчики')
--- shop_helper/models.py
+++ shop_helper/models.py
@@ -39,6 +39,7 @@ class Recipe(models.Model):
     class Meta:
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
+        ordering = ['-created_at']
 
 
 class RecipesProducts(models.Model):
