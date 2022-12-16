29595c37e28a017275dec5b24027b6ab2e57bd90
hqdem <ilyadem4@gmail.com>
2022-12-16 12:17:08 +0300

    add method get_serializer_class to all viewsets

--- shop_helper/viewsets.py
+++ shop_helper/viewsets.py
@@ -24,7 +24,7 @@ from .filters import RecipeByUserFilter, RecipeBySubscribersFilter
 
 
 class ProductViewSet(viewsets.ModelViewSet):
-    serializer_class = ProductSerializer
+    # serializer_class = ProductSerializer
     permission_classes = [IsAdminOrReadOnly]
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['category']
@@ -32,10 +32,16 @@ class ProductViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return Product.objects.select_related('category').all()
 
+    def get_serializer_class(self):
+        if self.action in ['create', 'update']:
+            return ProductCreateUpdateSerilizer
+        return ProductSerializer
+
     def create(self, request, *args, **kwargs):
         data = request.data
 
-        serializer = ProductCreateUpdateSerilizer(data=data)
+        # serializer = ProductCreateUpdateSerilizer(data=data)
+        serializer = self.get_serializer(data=data)
         if serializer.is_valid():
             product, created = serializer.save()
             if created:
@@ -47,7 +53,8 @@ class ProductViewSet(viewsets.ModelViewSet):
         data = request.data
         product = self.get_object()
 
-        serializer = ProductCreateUpdateSerilizer(product, data=data)
+        # serializer = ProductCreateUpdateSerilizer(product, data=data)
+        serializer = self.get_serializer(product, data=data)
         if serializer.is_valid():
             product = serializer.save()
             return Response(ProductSerializer(product).data, status=status.HTTP_200_OK)
@@ -56,20 +63,30 @@ class ProductViewSet(viewsets.ModelViewSet):
 
 class ShoppingListViewSet(viewsets.GenericViewSet):
     permission_classes = [permissions.IsAuthenticated]
-    serializer_class = ShoppingListSerializer
+    # serializer_class = ShoppingListSerializer
 
     def get_object(self):
         return get_object_or_404(ShoppingList.objects.prefetch_related('products__category').select_related('owner'),
                                  owner=self.request.user)
 
+    def get_serializer_class(self):
+        if self.action == 'create_list':
+            return ShoppingListCreateSerializer
+        if self.action in ['add_product', 'remove_product']:
+            return ProductToShoppingListSerializer
+
+        return ShoppingListSerializer
+
     @action(detail=False, methods=['get'])
     def get_list(self, request):
-        return Response(ShoppingListSerializer(self.get_object()).data, status=status.HTTP_200_OK)
+        # return Response(ShoppingListSerializer(self.get_object()).data, status=status.HTTP_200_OK)
+        return Response(self.get_serializer(self.get_object()).data, status=status.HTTP_200_OK)
 
     @action(detail=False, methods=['post'])
     def create_list(self, request):
         data = request.data
-        serializer = ShoppingListCreateSerializer(data=data)
+        # serializer = ShoppingListCreateSerializer(data=data)
+        serializer = self.get_serializer(data=data)
         if serializer.is_valid():
             serializer_data = serializer.data
             product_ids = [product_id['product_id'] for product_id in serializer_data['products']]
@@ -91,7 +108,8 @@ class ShoppingListViewSet(viewsets.GenericViewSet):
     @action(detail=False, methods=['post'])
     def add_product(self, request):
         data = request.data
-        serializer = ProductToShoppingListSerializer(data=data)
+        # serializer = ProductToShoppingListSerializer(data=data)
+        serializer = self.get_serializer(data=data)
         if serializer.is_valid():
             serializer_data = serializer.data
             product = get_object_or_404(Product, pk=serializer_data['product_id'])
@@ -103,7 +121,8 @@ class ShoppingListViewSet(viewsets.GenericViewSet):
     @action(detail=False, methods=['post'])
     def remove_product(self, request):
         data = request.data
-        serializer = ProductToShoppingListSerializer(data=data)
+        # serializer = ProductToShoppingListSerializer(data=data)
+        serializer = self.get_serializer(data=data)
         if serializer.is_valid():
             serializer_data = serializer.data
             product = get_object_or_404(Product, pk=serializer_data['product_id'])
@@ -116,13 +135,23 @@ class ShoppingListViewSet(viewsets.GenericViewSet):
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.prefetch_related('recipes_products', 'recipes_products__product',
                                                'recipes_products__product__category').select_related('owner').all()
-    serializer_class = RecipeSerializer
+    # serializer_class = RecipeSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     filter_backends = [RecipeByUserFilter, RecipeBySubscribersFilter]
 
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return RecipeCreateSerializer
+        if self.action == 'add_product':
+            return RecipeAddProductSerializer
+        if self.action == 'remove_product':
+            return RecipeRemoveProductSerializer
+        return RecipeSerializer
+
     def create(self, request, *args, **kwargs):
         data = request.data
-        serializer = RecipeCreateSerializer(data=data, context={'request': request})
+        # serializer = RecipeCreateSerializer(data=data, context={'request': request})
+        serializer = self.get_serializer(data=data, context={'request': request})
         if serializer.is_valid():
             recipe = serializer.save()
             return Response(self.get_serializer(recipe).data, status=status.HTTP_201_CREATED)
@@ -131,7 +160,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['post'])
     def add_product(self, request, pk):
         data = request.data
-        serializer = RecipeAddProductSerializer(data=data, context={'pk': pk})
+        # serializer = RecipeAddProductSerializer(data=data, context={'pk': pk})
+        serializer = self.get_serializer(data=data, context={'pk': pk})
         if serializer.is_valid():
             product_recipe, created = serializer.save()
             if created:
@@ -141,7 +171,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['post'])
     def remove_product(self, request, pk):
         data = request.data
-        serializer = RecipeRemoveProductSerializer(data=data)
+        # serializer = RecipeRemoveProductSerializer(data=data)
+        serializer = self.get_serializer(data=data)
         if serializer.is_valid():
             recipe_id = pk
             product_id = serializer.validated_data['product_id']
